4118e0896acf6c09257f3536d29081a58d604cba
Hendrik Künnemann <159906011+hkuennemann@users.noreply.github.com>
2025-06-10 22:54:42 +0100

    Road2Cary Lib

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LibRoad2Cary',
+    packages=find_packages(),
+    description='Short description of your library',  # TODO
+    long_description='A longer description of your library', #TODO
+    author='Hendrik Künnemann'
+)
