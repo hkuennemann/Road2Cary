3a1bd4cb539244ab4448ba6d26b293008319b31e
Hendrik Kunnemann <hendrik.kunnemann@sas.com>
2025-06-13 11:38:33 +0100

    Testing introduction of more flexibility - data

---
+++ LibRoad2Cary/data/datasets.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+TEAM_PROGRESS_DF = None
+
+def get_data(date: str):
+    try:
+        file_path = os.path.join(
+            r"C:\Users\hekunn\OneDrive - SAS\Documents\Road2Cary\Road2Cary\Data",
+            date,
+            "team_progress.csv"
+        )
+        return pd.read_csv(file_path)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}.")
+        print("Check if the date input has the format: 'MM_DD_YY', e.g., '06_12_25'.")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
